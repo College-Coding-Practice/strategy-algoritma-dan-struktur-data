d232ed9c64498c798241462273a3dc69dc5f55ae
Mas 'Edi <85278436+masedi54@users.noreply.github.com>
2023-03-12 18:04:37 +0700

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,8 @@
+import math
+a = int (input('masukkan a:'))
+b = int (input('masukkan b:'))
+c = int (input('masukkan c:'))
+d = (b**2) - (4*a*c)
+x1 = (-b+math.sqrt(d))/(2*a)
+x2 = (-b-math.sqrt(d))/(2*a)
+print('solusinya adalah {0} dan {1}'.format(x1.x2))
